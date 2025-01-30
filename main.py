c50eafc137cfc348f83b943ef26957c64f380596
AstridBrogren <astrid_brogren@biswashington.org>
2025-01-30 17:46:12 +0000

    done

--- main.py
+++ main.py
@@ -1,2 +1,14 @@
-print("hello")
-name = input("enter your name: ")
+print("hello and welcome to your licence test")
+name = input("enter your full name: ")
+age = int(input("enter your age: "))
+if age <16:
+    print("you are to young to drive")
+elif age >16:
+    print("you are old enough")
+
+print("if you beat me in rock, paper, sissors you can get your licence")
+
+
+from random import *
+selection
+
